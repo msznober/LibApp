59e24c83c9f71f678b46f90e606fbc432c8507fa
joann <joann@LAPTOP-K0QD016U>
2022-02-16 19:57:48 +0100

    Dodaj pliki projektów.

---
+++ Controllers/Api/CustomersController.cs
@@ -0,0 +1,112 @@
+﻿using AutoMapper;
+using LibApp.Data;
+using LibApp.Dtos;
+using LibApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using HttpDeleteAttribute = Microsoft.AspNetCore.Mvc.HttpDeleteAttribute;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
+using HttpPutAttribute = Microsoft.AspNetCore.Mvc.HttpPutAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace LibApp.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        public CustomersController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET /api/customers
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            var customers = _context.Customers
+                                        .Include(c => c.MembershipType)
+                                        .ToList()
+                                        .Select(_mapper.Map<Customer, CustomerDto>);
+            return Ok(customers);
+        }
+
+        // GET /api/customers/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomer(int id)
+        {
+            Console.WriteLine("START REQUEST");
+            var customer = await _context.Customers.SingleOrDefaultAsync(c => c.Id == id);
+            await Task.Delay(2000);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            
+            Console.WriteLine("END REQUEST");
+            return Ok(_mapper.Map<CustomerDto>(customer));
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public CustomerDto CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var customer = _mapper.Map<Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            customerDto.Id = customer.Id;
+
+            return customerDto;
+        }
+
+        // PUT /api/customers/{id}
+        [HttpPut("{id}")]
+        public void UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _mapper.Map(customerDto, customerInDb);
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/customers
+        [HttpDelete("{id}")]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+        }
+
+        private ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using LibApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            DateTime currentTime;
+            bool existsInCache = _memoryCache.TryGetValue("CachedTime", out currentTime);
+            if (!existsInCache)
+            {
+                currentTime = DateTime.Now;
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                {
+                    AbsoluteExpiration = DateTime.Now.AddSeconds(5),
+                    Priority = CacheItemPriority.High,
+                    SlidingExpiration = TimeSpan.FromSeconds(5)
+                };
+
+                _memoryCache.Set("CachedTime", currentTime, cacheEntryOptions);
+            }
+
+            return View(currentTime);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private readonly IMemoryCache _memoryCache;
+    }
+}
