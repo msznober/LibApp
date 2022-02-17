ed8534515e82d7b707cf22084025054d5e57f4cb
Mateusz Sznober <mateusz.sznober@microsoft.wsei.edu.pl>
2022-02-17 19:25:33 +0100

    Task 2 - validation & anti-forgery token

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,21 +9,34 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-		[Required]
+		
+		[Required(ErrorMessage = "Name is required")]
 		[StringLength(255)]
 		public string Name { get; set; }
-		[Required]
+
+		[Required(ErrorMessage = "Author name is required")]
 		public string AuthorName { get; set; }
-		[Required]
+
+		[Required(ErrorMessage = "Genre is required")]
 		public Genre Genre { get; set; }
-		[Required]
+		
+		[Required(ErrorMessage = "Genre ID is required")]
 		[Display(Name="Genre")]
 		public byte GenreId { get; set; }
+		
+		[Required(ErrorMessage = "Date added is required")]
 		public DateTime DateAdded { get; set; }
+
+		[Required(ErrorMessage = "Relase date is required")]
 		[Display(Name="Release Date")]
 		public DateTime ReleaseDate { get; set; }
+
+		[Range(1, 20, ErrorMessage = "Number in stock must be between 1 and 20")]
 		[Display(Name = "Number in Stock")]
+		[Required(ErrorMessage = "Number in stock is required")]
 		public int NumberInStock { get; set; }
+		
+		[Required(ErrorMessage = "Number available is required")]
 		public int NumberAvailable { get; set; }
 	}
       
