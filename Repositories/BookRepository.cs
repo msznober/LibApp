3ff2e0475f21f5afa4a3c60a923d2e11108965f8
Mateusz Sznober <mateusz.sznober@microsoft.wsei.edu.pl>
2022-02-17 19:48:11 +0100

    Task 3 - implement interface

---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,34 @@
+﻿using LibApp.Data;
+using LibApp.Interfaces;
+using LibApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibApp.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Book> GetBooks()
+        {
+            return _context.Books;
+        }
+
+        Book GetBookById(int id) => _context.Books.Find(id);
+
+        void AddBook(Book book) => _context.Books.Add(book);
+
+        void DeleteBook(int id) => _context.Books.Remove(GetBookById(id));
+
+        void UpdateBook(Book book) => _context.Books.Update(book);
+
+        void Save() => _context.SaveChanges();
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibApp.Interfaces;
+using LibApp.Repositories;
 
 namespace LibApp
 {
@@ -36,6 +38,7 @@ namespace LibApp
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddScoped<IBookRepository, BookRepository>();
             services.AddControllersWithViews();
         }
 
